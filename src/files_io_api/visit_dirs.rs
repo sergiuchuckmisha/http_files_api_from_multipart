893819a3fe32ae67e8ec992f946d09991f8a4bee
sergiuchuckmisha <caga3@inbox.ru>
2018-06-19 12:40:18 +0300

    modify function signature to find files in directory recursively

--- src/files_io_api/visit_dirs.rs
+++ src/files_io_api/visit_dirs.rs
@@ -7,9 +7,9 @@ use std::fs::{self, DirEntry};
 use std::path::Path;
 
 // one possible implementation of walking a directory only visiting files
-pub fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
+pub fn visit_dirs<P: AsRef<Path>>(dir_path: P, cb: &Fn(&DirEntry)) -> io::Result<()> {
+    if dir_path.as_ref().is_dir() {
+        for entry in fs::read_dir(dir_path)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
@@ -30,11 +30,11 @@ mod tests {
 
     #[test]
     fn test_visit_dirs_print_hw() {
-        visit_dirs(&Path::new(TEMPORARY_FOLDER_PATH), &|_|{println!("works!")});
+        visit_dirs(TEMPORARY_FOLDER_PATH, &|_|{println!("works!")});
     }
 
     #[test]
     fn test_visit_dirs_print_path() {
-        visit_dirs(&Path::new(TEMPORARY_FOLDER_PATH), &|ref entry|{println!("{:?}", entry.path())});
+        visit_dirs(TEMPORARY_FOLDER_PATH, &|ref entry|{println!("{:?}", entry.path())});
     }
 }
